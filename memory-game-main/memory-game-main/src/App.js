b84fd9f0d5e11147822313e66552ba00473e4f27
THAMER ALSHMALE <116897896+1tvsv@users.noreply.github.com>
2022-11-08 23:30:56 +0300

    Add files via upload

---
+++ memory-game-main/memory-game-main/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>لعبة الذاكرة</h2>
+      <button>بدء لعبة جديده</button>
+    </div>
+  );
+}
+
+export default App;
